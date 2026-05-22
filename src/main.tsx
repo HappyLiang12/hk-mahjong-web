d78d4b37cb18ee6cd11eb9efe048a01a814393d7
Happy Liang <happyl@users.noreply.github.com>
2026-05-22 18:00:37 +0800

    fix: remove trailing slash from BrowserRouter basename per React Router v6 spec

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/hk-mahjong-web/">
+    <BrowserRouter basename="/hk-mahjong-web">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
